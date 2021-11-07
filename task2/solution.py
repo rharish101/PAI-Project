2be10e06a339b7389e223f40bef69479ec1203d8
Harish Rajagopal <harish.rajagopals@gmail.com>
2021-11-07 23:07:44 +0100

    Increased epochs to 200

--- task2/solution.py
+++ task2/solution.py
@@ -58,7 +58,7 @@ class Model(object):
     def __init__(self):
         # Hyperparameters and general parameters
         # You might want to play around with those
-        self.num_epochs = 100  # number of training epochs
+        self.num_epochs = 200  # number of training epochs
         self.batch_size = 128  # training batch size
         learning_rate = 1e-3  # training learning rates
         hidden_layers = (100, 100)  # for each entry, creates a hidden layer with the corresponding number of units
