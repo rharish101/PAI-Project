7aef9c0732748ea794aa11f18863396d3fc3f669
Harish Rajagopal <harish.rajagopals@gmail.com>
2021-11-08 18:56:04 +0100

    Simplified code using an existing variable

--- task2/solution.py
+++ task2/solution.py
@@ -126,7 +126,7 @@ class Model(object):
 
                     # Implement Bayes by backprop training here
                     current_logits, log_prior, log_variational_posterior = self.network(batch_x)
-                    minibatch_likelihood = 2 ** (len(train_loader) - batch_idx - 1) / (2 ** len(train_loader) - 1)
+                    minibatch_likelihood = 2 ** (num_batches - batch_idx - 1) / (2 ** num_batches - 1)
                     loss = F.nll_loss(F.log_softmax(current_logits, dim=1), batch_y, reduction='sum')
                     loss += minibatch_likelihood * (log_variational_posterior - log_prior)
 
